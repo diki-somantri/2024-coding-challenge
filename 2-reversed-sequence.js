79f66d00fd7b7142314e11e245962f3dc8e10249
“diksom” <diki_somantri@ymail.com>
2024-03-14 14:07:54 +0800

    day 2/366

---
+++ 2-reversed-sequence.js
@@ -0,0 +1,25 @@
+//Coding Challenge 2024
+//2/366
+// https://www.codewars.com/kata/5a00e05cc374cb34d100000d
+// const reverseSeq = (n) => {
+//   const result = [];
+//   for (let i = n; i >= 1; i--) {
+//     result.push(i);
+//   }
+//   return result;
+// };
+
+// reverseSeq = (n) => {
+//     return Array.from({ length: n }, (_, i) => i + 1).reverse();
+// }
+
+// reverseSeq = (n) => {
+//   return [...Array(n)].map((el, i) => i + 1).reverse();
+// };
+
+// const reverseSeq = (n) => [...Array(n)].map((el, i) => i + 1).reverse();
+
+const reverseSeq = (n) => 
+    [...Array(n)].map((el, i) => n-i);
+
+console.log(reverseSeq(5));
