fab5975ecec60802b85d97b41c9b33881da6272f
“diksom” <diki_somantri@ymail.com>
2024-03-14 23:15:04 +0800

    day 10/366

---
+++ 10-stringToArray.js
@@ -0,0 +1,15 @@
+//Coding Challenge 2024
+//10/366
+// https://www.codewars.com/kata/57e76bc428d6fbc2d500036d
+
+// const stringToArray = (str) => {
+//   return str.split(" ");
+// };
+
+// function stringToArray(string) {
+//     return string.split(" ");
+// }
+
+const stringToArray = (str) => str.split(" ");
+
+console.log(stringToArray("Robin Singh"));
